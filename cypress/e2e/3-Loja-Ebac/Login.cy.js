7c5045d50d644e177411f6756028875edf2d880a
Nathan <nathanbrlima@gmail.com>
2025-02-26 11:15:52 -0300

    commit segundo teste

--- cypress/e2e/3-Loja-Ebac/Login.cy.js
+++ cypress/e2e/3-Loja-Ebac/Login.cy.js
@@ -10,4 +10,13 @@ describe('Funcionalidade: Login',() =>{
 
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, cy.testes (não é cy.testes? Sair)')
     })
+
+    it('Deve inserir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username'). type('cy.testar@testes.com')
+        cy.get('#password'). type('@testes123')
+        cy.get('.woocommerce-form > .button').click()
+
+        
+    });
 })
