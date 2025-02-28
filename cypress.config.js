5b65c5ea56c2baaa4dc80b83f4f9fa3813e157d4
Nathan <nathanbrlima@gmail.com>
2025-02-28 15:13:05 -0300

    teste relatórios

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "nd1w16",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://lojaebac.ebaconline.art.br/'
+    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
   },
 });
